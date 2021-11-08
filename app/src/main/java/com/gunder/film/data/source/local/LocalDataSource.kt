1177bb606873f69ed195a4a9818c6ce1482edd17
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-08 16:32:37 +0700

    membuat LocalDataSource.kt

---
+++ app/src/main/java/com/gunder/film/data/source/local/LocalDataSource.kt
@@ -0,0 +1,34 @@
+package com.gunder.film.data.source.local
+
+import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
+import com.gunder.film.data.source.local.entity.DetailEntity
+import com.gunder.film.data.source.local.entity.ListEntity
+import com.gunder.film.data.source.local.room.FilmDao
+
+
+class LocalDataSource private constructor(private val mFilmDao: FilmDao) {
+    companion object {
+        private var INSTANCE: LocalDataSource? = null
+
+        fun getInstance(filmDao: FilmDao): LocalDataSource =
+            INSTANCE ?: LocalDataSource(filmDao).apply { INSTANCE = this }
+    }
+
+    fun getMovies(): DataSource.Factory<Int, ListEntity> = mFilmDao.getMovies()
+
+    fun getTvShow(): DataSource.Factory<Int, ListEntity> = mFilmDao.getTvShow()
+
+    fun getFavorited(): DataSource.Factory<Int, ListEntity> = mFilmDao.getFavorite()
+
+    fun insertFilm(film: List<ListEntity>) = mFilmDao.insertFilm(film)
+
+    fun getDetailById(id: Int): LiveData<DetailEntity> = mFilmDao.getDetailById(id)
+
+    fun insertDetail(film: DetailEntity) = mFilmDao.insertDetailFilm(film)
+
+    fun setFilmFavorite(film: ListEntity, newState: Boolean) {
+        film.favorited = newState
+        mFilmDao.updateFilm(film)
+    }
+}
